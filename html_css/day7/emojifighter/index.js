3f1d0da74ec98b72c7b8f699abd6d64c2850d083
Sindhu <Sindhupriyakamaraj@gmail.com>
2024-04-27 21:57:51 +0530

    password_generator

--- html_css/day7/emojifighter/index.js
+++ html_css/day7/emojifighter/index.js
@@ -1,10 +1,27 @@
-let fighters = ["🐉", "🐥", "🐊","💩", "🦍", "🐢", "🐩", "🦭", "🦀", "🐝", "🤖", "🐘", "🐸", "🕷","🐆", "🦕", "🦁"]
+// let fighters = ["🐉", "🐥", "🐊","💩", "🦍", "🐢", "🐩", "🦭", "🦀", "🐝", "🤖", "🐘", "🐸", "🕷","🐆", "🦕", "🦁"]
 
-let stageEl = document.getElementById("stage")
-let fightButton = document.getElementById("fightButton")
+// let stageEl = document.getElementById("stage")
+// let fightButton = document.getEl'ementById("fightButton")
 
-fightButton.addEventListener("click", function() {
-    let randomIndexOne = Math.floor( Math.random() * fighters.length )
-    let randomIndexTwo = Math.floor( Math.random() * fighters.length )
-    stageEl.textContent = fighters[randomIndexOne] + " vs " + fighters[randomIndexTwo]
-})
+// fightButton.addEventListener("click", function() {
+//     let randomIndexOne = Math.floor( Math.random() * fighters.length )
+//     let randomIndexTwo = Math.floor( Math.random() * fighters.length )
+//     stageEl.textContent = fighters[randomIndexOne] + " vs " + fighters[randomIndexTwo]
+// })
+let fruit = ["🍎", "🍊", "🍎", "🍎", "🍊"]
+let appleShelf = document.getElementById("apple-shelf")
+let orangeShelf = document.getElementById("orange-shelf")
+function arrFruits()
+{
+    for(i=0;i<fruit.length;i++)
+    {
+        if (fruit[i]==="🍎")
+        {
+            appleShelf.innerHTML += " 🍎"
+        } 
+        else
+        {
+            orangeShelf.innerHTML+=" 🍊"
+        }
+    }
+}
---
+++ html_css/day7/passwordgenerator/index.js
@@ -0,0 +1,14 @@
+
+        function generatePassword() {
+            const length = 12; // Length of the password
+            const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+"; // Characters to include in the password
+            let password = "";
+
+            for (let i = 0; i < length; i++) {
+                const randomIndex = Math.floor(Math.random() * charset.length);
+                password += charset[randomIndex];
+            }
+
+            document.getElementById("password").innerText = password;
+        }
+    
