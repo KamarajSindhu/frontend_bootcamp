9b48e393cdff5fccab0574495e97ed8a1773b574
Sindhu <Sindhupriyakamaraj@gmail.com>
2024-05-01 04:45:39 +0530

    variable_add

--- html_css/day10/template_string/index.js
+++ html_css/day10/template_string/index.js
@@ -1,5 +1,6 @@
 const recipient = "James"
+const sender = "sindhu"
 // const email = "Hey " + recipient + "! How is it going? Cheers Per"
 //string templates : `${recipient}`
-const email = `Hey ${recipient} ! How is it going? Cheers Per`
+const email = `Hey ${recipient} ! How is it going? Cheers ${sender}`
 console.log(email)
