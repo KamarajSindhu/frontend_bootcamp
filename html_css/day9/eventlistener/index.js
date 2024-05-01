82f48f2bf528227902e4e0d0a7a1494a42e63bcc
Sindhu <Sindhupriyakamaraj@gmail.com>
2024-05-01 21:30:41 +0530

    chrome_extension_completed

--- html_css/day9/eventlistener/index.js
+++ html_css/day9/eventlistener/index.js
@@ -1,41 +1,35 @@
-let myLeads=[];
-let inputEl=document.getElementById("input-el")
-let inputBtn = document.getElementById("input-btn")
-let ulEl = document.getElementById("ul-el")
-let deleteBtn= document.getElementById("delete-btn")
+let myLeads=[]
+const inputEl=document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
+const deleteBtn= document.getElementById("delete-btn")
+const tabBtn = document.getElementById("tab-btn")
 const leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
 if(leadsFromLocalStorage){
-    myLeads=leadsFromLocalStorage
-    renderLeads();
+    myLeads = leadsFromLocalStorage
+    render(myLeads);
 }
 
-deleteBtn.addEventListener("dblclick", function() {
-    console.log("double clicked!")
-    localStorage.clear()
-    myLeads = []
-    renderLeads();
+tabBtn.addEventListener("click", function(){
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        myLeads.push(tabs[0].url)
+    localStorage.setItem("myLeads",JSON.stringify(myLeads)) 
+    render(myLeads);
+    });
+
+    
 })
 
-
-inputBtn.addEventListener("click",function()
+ function render(leads)
 {
-    myLeads.push(inputEl.value);
-    ulEl.innerHTML +=`<li><a target='_blank' href='#'> ${inputEl.value} </a></li>`
-    inputEl.value="";    
-    localStorage.setItem("myLeads",JSON.stringify(myLeads))
-    console.log(localStorage.getItem("myLeads"))
-}); 
-function renderLeads()
-{
-    let listItems = "<li>" +inputEl.value + "</li>"
-    ulEl.innerHTML +=listItems;
+    let listItems = ""
 
-for(let i=0;i<myLeads.length;i++)
+for(let i=0;i<leads.length;i++)
 {
     listItems += `
             <li>
-                <a target='_blank' href='${myLeads[i]}'>
-                    ${myLeads[i]}
+                <a target='_blank' href='${leads[i]}'>
+                    ${leads[i]}
                 </a>
             </li>
         `
@@ -43,3 +37,20 @@ for(let i=0;i<myLeads.length;i++)
 ulEl.innerHTML = listItems  
 
 }
+
+
+
+deleteBtn.addEventListener("dblclick", function() {
+    localStorage.clear()
+    myLeads = []
+    render(myLeads);
+})
+
+
+inputBtn.addEventListener("click",function()
+{
+    myLeads.push(inputEl.value);
+    inputEl.value="";    
+    localStorage.setItem("myLeads",JSON.stringify(myLeads))
+    render(myLeads)
+}); 
