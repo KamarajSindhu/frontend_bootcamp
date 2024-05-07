4c11debab89a128ee77f2f9321e1a792b6e21c2a
Sindhu <Sindhupriyakamaraj@gmail.com>
2024-05-07 23:11:36 +0530

    challenges on arraytoobjects

---
+++ html_css/turningArrayintoObjects/index.js
@@ -0,0 +1,17 @@
+let scrimbaUsers = {
+    "00": "sindre@scrimba.com",
+    "01": "per@scrimba.com",
+    "02": "frode@scrimba.com"
+}
+//Create a let variable called 'scrimbaUsersEmails' and use one of Object methods to set it equal to an array with the values
+
+let scrimbaUsersEmails = Object.values(scrimbaUsers)
+  
+console.log(scrimbaUsersEmails);
+// Create a let variable called 'scrimbaUsersIDs' and use one of Object methods to set it equal to an array with the keys
+let scrimbaUsersIDs= Object.keys(scrimbaUsers)
+console.log(scrimbaUsersIDs);
+
+// Create a let variable called 'scrimbaUsersEntries' and use one of Object methods to set it equal to an array with the both the keys and values
+let scrimbaUsersEntries =Object.entries(scrimbaUsers)
+console.log(scrimbaUsersEntries);
