64e6fbae5a20416ad7d8a40a4fe1c298aa0b3a91
Sindhu <Sindhupriyakamaraj@gmail.com>
2024-05-07 21:44:44 +0530

    restoring

--- html_css/styling_mobileapp/index.js
+++ html_css/styling_mobileapp/index.js
@@ -1,9 +1,10 @@
-import {initializeApp} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
-import { getDatabase, ref,push} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
-const appSettings={
-    databaseURL: "https://realtime-database-124de-default-rtdb.asia-southeast1.firebasedatabase.app/"
+const appSettings = {
+    databaseURL: "https://realtime-database-df319-default-rtdb.europe-west1.firebasedatabase.app/"
 }
+
 const app = initializeApp(appSettings)
 const database = getDatabase(app)
 const shoppingListInDB = ref(database, "shoppingList")
@@ -12,11 +13,22 @@ const inputFieldEl = document.getElementById("input-field")
 const addButtonEl = document.getElementById("add-button")
 const shoppingListEl = document.getElementById("shopping-list")
 
-
 addButtonEl.addEventListener("click", function() {
     let inputValue = inputFieldEl.value
-    push(shoppingListInDB, inputValue)
-    shoppingListEl.innerHTML += ` <li>${inputValue}</li>`
-
     
+    push(shoppingListInDB, inputValue)
+    
+    clearInputFieldEl()
+
+    appendItemToShoppingListEl(inputValue)
 })
+//restoring the one line code into function//
+
+function clearInputFieldEl() {
+    inputFieldEl.value = ""
+}
+//restoring the one line code into function//
+
+function appendItemToShoppingListEl(itemValue) {
+    shoppingListEl.innerHTML += `<li>${itemValue}</li>`
+}
