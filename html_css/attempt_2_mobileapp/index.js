a4142cdc360cab695b9520a32ae6a3c46d789ab8
Sindhu <Sindhupriyakamaraj@gmail.com>
2024-05-08 22:33:12 +0530

    mobile_app_design

---
+++ html_css/attempt_2_mobileapp/index.js
@@ -0,0 +1,7 @@
+let inputEl = document.getElementById("input-field")
+let btn =  document.getElementById("clicked")
+btn.addEventListener("click", function(){
+    let inputvalue = inputEl.value
+    console.log(inputvalue)
+}
+) 
