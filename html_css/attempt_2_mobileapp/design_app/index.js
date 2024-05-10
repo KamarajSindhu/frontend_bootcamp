ad19be5c181934e23823bee23eba06a3d1035a01
Sindhu <Sindhupriyakamaraj@gmail.com>
2024-05-10 20:19:16 +0530

    data_pushed_into DB

--- html_css/attempt_2_mobileapp/design_app/index.js
+++ html_css/attempt_2_mobileapp/design_app/index.js
@@ -1,16 +1,17 @@
 import{initializeApp} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
-import{getDatabase,  ref} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+import{getDatabase, ref, push} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 // importing the database
 const appSettings = {
     databaseURL: "https://realtime-database-afee4-default-rtdb.asia-southeast1.firebasedatabase.app/"
 }
 const app = initializeApp(appSettings)
 const database = getDatabase(app)
-const shopppingListInDB = ref(database,"ShoppingList")
-let inputEl = document.getElementById("input-field")
-let btn =  document.getElementById("clicked")
+const shopppingListInDB = ref(database,"shoppingList")
+const inputEl = document.getElementById("input-field")
+const btn =  document.getElementById("clicked")
 btn.addEventListener("click", function(){
-    let inputvalue = inputEl.value
-    console.log(inputvalue)
+    let inputvalue = inputEl.value;
+    //push data into db
+    push(shopppingListInDB,inputvalue)
 }
 ) 
