b808c230a852dd36e53f57057864d7d45fddd162
Sindhu <Sindhupriyakamaraj@gmail.com>
2024-05-13 07:34:42 +0530

    listing_tag

--- html_css/attempt_2_mobileapp/design_app/index.js
+++ html_css/attempt_2_mobileapp/design_app/index.js
@@ -8,10 +8,12 @@ const app = initializeApp(appSettings)
 const database = getDatabase(app)
 const shopppingListInDB = ref(database,"shoppingList")
 const inputEl = document.getElementById("input-field")
+const shoppingList =document.getElementById("shopping-list")
 const btn =  document.getElementById("clicked")
 btn.addEventListener("click", function(){
     let inputvalue = inputEl.value;
     //push data into db
     push(shopppingListInDB,inputvalue)
+    shoppingList.innerHTML += `<li>${inputvalue}</li>`
 }
 ) 
