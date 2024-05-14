d1d73efc1d0fcad491e1d60789f615b28c010377
Sindhu <Sindhupriyakamaraj@gmail.com>
2024-05-14 21:20:07 +0530

    data into database

--- html_css/attempt_2_mobileapp/design_app/index.js
+++ html_css/attempt_2_mobileapp/design_app/index.js
@@ -1,45 +1,46 @@
-// import{initializeApp} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
-// import{getDatabase, ref, push} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
-// // importing the database
-// const appSettings = {
-//     databaseURL: "https://realtime-database-afee4-default-rtdb.asia-southeast1.firebasedatabase.app/"
-// }
-// const app = initializeApp(appSettings)
-// const database = getDatabase(app)
-// const shopppingListInDB = ref(database,"shoppingList")
-// const inputEl = document.getElementById("input-field")
-// const shoppingList =document.getElementById("shopping-list")
-// const btn =  document.getElementById("clicked")
-// btn.addEventListener("click", function(){
-//     let inputvalue = inputEl.value;    
-//     //push data into db
-//     push(shopppingListInDB,inputvalue)
-//     clearInputField();
-//     appendItemToShoppingListEl(inputvalue);
-// }
-// ) 
-
-// function clearInputField(){
-
-//     inputEl.value = "";
-
-// }
-// function appendItemToShoppingListEl(itemvalue)
-// {
-//     shoppingList.innerHTML += `<li>${itemvalue}</li>`
-// }
-
-let scrimbaUsers = {
-    "00": "sindre@scrimba.com",
-    "01": "per@scrimba.com",
-    "02": "frode@scrimba.com"
+import{initializeApp} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import{getDatabase, ref, push} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+// importing the database
+const appSettings = {
+    databaseURL: "https://realtime-database-afee4-default-rtdb.asia-southeast1.firebasedatabase.app/"
 }
-let scrimbaUsersEmails = Object.values(scrimbaUsers);
-console.log(scrimbaUsersEmails); 
-let scrimbaUsersID = Object.keys(scrimbaUsers);
-console.log(scrimbaUsersID);
-let scrimbaUsersEntries = Object.entries(scrimbaUsers);
-console.log(scrimbaUsersEntries); 
+const app = initializeApp(appSettings)
+const database = getDatabase(app)
+const shopppingListInDB = ref(database,"shoppingList")
+const inputEl = document.getElementById("input-field")
+const shoppingList =document.getElementById("shopping-list")
+const btn =  document.getElementById("clicked")
+btn.addEventListener("click", function(){
+    let inputvalue = inputEl.value;    
+    //push data into db
+    push(shopppingListInDB,inputvalue)
+    clearInputField();
+    appendItemToShoppingListEl(inputvalue);
+}
+) 
+
+function clearInputField(){
+
+    inputEl.value = "";
+
+}
+function appendItemToShoppingListEl(itemvalue)
+{
+    shoppingList.innerHTML += `<li>${itemvalue}</li>`
+}
+
+// Object into an array//
+// let scrimbaUsers = {
+//     "00": "sindre@scrimba.com",
+//     "01": "per@scrimba.com",
+//     "02": "frode@scrimba.com"
+// }
+// let scrimbaUsersEmails = Object.values(scrimbaUsers);
+// console.log(scrimbaUsersEmails);
+// let scrimbaUsersID = Object.keys(scrimbaUsers);
+// console.log(scrimbaUsersID);
+// let scrimbaUsersEntries = Object.entries(scrimbaUsers);
+// console.log(scrimbaUsersEntries);
 
 
 
