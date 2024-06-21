242105c78bd6cf6f4761f8ad4877ab2dcef41259
Sindhu <Sindhupriyakamaraj@gmail.com>
2024-06-21 22:11:27 +0530

    preventDefault feature

--- html_css/annoying_cookie consent/index.js
+++ html_css/annoying_cookie consent/index.js
@@ -1,13 +1,17 @@
-const modal = document.getElementById('modal');
-const modalCloseBtn = document.getElementById('modal-close-btn');
-
+const modal = document.getElementById('modal')
+const modalCloseBtn = document.getElementById('modal-close-btn')
+const consentForm = document.getElementById('consent-form')
 
 setTimeout(function(){
     modal.style.display = 'inline'
 }, 1500)
 
-modalCloseBtn.addEventListener('click', myFunction)
-function myFunction(){
-    modal.style.display = 'none'    
-}
+modalCloseBtn.addEventListener('click', function(){
+    modal.style.display = 'none'
+})
 
+
+consentForm.addEventListener('submit', function(e){
+    e.preventDefault()
+    console.log('form submitted')
+})
