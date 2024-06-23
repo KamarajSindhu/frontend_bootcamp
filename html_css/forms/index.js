5aae4f7d49edf20dda0384a2cc19babaa64b0c53
Sindhu <Sindhupriyakamaraj@gmail.com>
2024-06-23 21:11:27 +0530

    FormGetData method

---
+++ html_css/forms/index.js
@@ -0,0 +1,13 @@
+const loginForm = document.getElementById('login-form')
+
+loginForm.addEventListener('submit', function(e){
+    e.preventDefault()
+    const loginFormData = new FormData(loginForm)
+    
+    const name = loginFormData.get('astronautName')
+    console.log(name)
+
+    const email = loginFormData.get('astronautEmail')
+    console.log(email)
+
+})
