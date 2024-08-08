09f02f6b2f2bc771a24104c9e7b52c8525a1df3c
Sindhu <Sindhupriyakamaraj@gmail.com>
2024-08-08 09:43:08 +0530

    file not found error

--- html_css/essential_js_challenge/map method/index.js
+++ html_css/essential_js_challenge/map method/index.js
@@ -1,28 +1,19 @@
 import { playlistArr } from '/playlist.js'
-
-/*
-Challenge
-1. Refactor the code below to use .map() 
-   instead of the for loop.
-   ⚠️ Don't worry about the commas for now.
-*/
-
-const playlistHtml = playlistArr.map(function(track){
-    return `<section class="card">
+const playlistHtml = playlistArr.map(function (track) {
+    return `
+<section class="card">
     <div class="card-start">
-        <img src="assets/${playlistArr[track].albumArt}">
+        <img src="/images/${track.albumArt}">
     </div>
         <div class="card-mid">
-            <h4 class="card-title">${playlistArr[track].title}</h4>
-            <p class="card-artist">${playlistArr[track].artist}</p>
+            <h4 class="card-title">${track.title}</h4>
+            <p class="card-artist">${track.artist}</p>
         </div>
     <div class="card-end">
         <p class="card-menu">...</p>
     </div>
 </section>
-    `
-
+`
 })
-
 
 document.getElementById('container').innerHTML = playlistHtml
--- html_css/essential_js_challenge/map method/playlist.js
+++ html_css/essential_js_challenge/map method/playlist.js
@@ -1,22 +1,19 @@
-export const playlistArr = [
-    {
-        title: 'Bohemian Rhapsody',
-        artist: 'Queen',
-        albumArt: 'bohemian-rhapsody.png'
-    },
-    {
-        title: 'As It Was',
-        artist: 'Harry Styles',
-        albumArt: 'as-it-was.png'
-    },
-    {
-        title: 'Stairway to Heaven',
-        artist: 'Led Zeppelin',
-        albumArt: 'stairway-to-heaven.png'
-    },
-    {
-        title: 'Therefore I Am',
-        artist: 'Billie Eilish',
-        albumArt: 'therefore-i-am.png'
-    }
-]
+import { playlistArr } from 'C:\Users\sindh\OneDrive\Documents\GitHub\frontend_bootcamp\html_css\essential_js_challenge\map method\playlist.js'
+const playlistHtml = playlistArr.map(function (track) {
+    return `
+<section class="card">
+    <div class="card-start">
+        <img src="/images/${track.albumArt}">
+    </div>
+        <div class="card-mid">
+            <h4 class="card-title">${track.title}</h4>
+            <p class="card-artist">${track.artist}</p>
+        </div>
+    <div class="card-end">
+        <p class="card-menu">...</p>
+    </div>
+</section>
+`
+})
+
+document.getElementById('container').innerHTML = playlistHtml
