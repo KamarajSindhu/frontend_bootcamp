abde9054027c3a227244ecffee1069ff4389f858
Sindhu <Sindhupriyakamaraj@gmail.com>
2024-08-14 21:45:03 +0530

    used arrow function

---
+++ javascript/essential_js_mini_projects/arrow function challenge/index.js
@@ -0,0 +1,13 @@
+// function speedWarning(speed){
+//     return `You are going at ${speed} mph!`
+// }
+
+// console.log(speedWarning(40))
+
+const speedWarning=(speed) =>{
+    return `You are going at ${speed} mph!`
+
+}
+console.log(speedWarning(100))
+
+
