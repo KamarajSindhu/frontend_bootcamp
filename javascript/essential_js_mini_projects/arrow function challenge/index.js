7a04a04a71174daa97045ad4402c838772af6b0a
Sindhu <Sindhupriyakamaraj@gmail.com>
2024-08-14 21:58:00 +0530

    passing two parameters and returning the speed limit and actual speed

--- javascript/essential_js_mini_projects/arrow function challenge/index.js
+++ javascript/essential_js_mini_projects/arrow function challenge/index.js
@@ -4,7 +4,11 @@
 
 // console.log(speedWarning(40))
 
-const speedWarning=(speed) => `You are going at ${speed} mph!`
-console.log(speedWarning(100))
+const speedWarning=(speedlimit,speed) => {
+
+    return `You are going at ${speed} mph!.The ${speedlimit} mph! should be drive limit`
+
+}
+console.log(speedWarning(30,40))
 
 
