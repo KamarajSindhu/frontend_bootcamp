90001a3563fe3db5c93d0494168a33e55c0e1410
Sindhu <Sindhupriyakamaraj@gmail.com>
2024-09-24 08:58:19 +0530

    close the modal by using event listener

--- javascript/New Slate/world famous annoying website/index.js
+++ javascript/New Slate/world famous annoying website/index.js
@@ -1,9 +1,14 @@
 
-
+const modal = document.getElementById("modal")
+const modalCloseBtn = document.getElementById("modal-close-btn")
 
 setTimeout (function(){
-    const modal = document.getElementById("modal")
     modal.style.display='inline'
-
 },1500)
 
+modalCloseBtn.addEventListener('click', function(){
+    modal.style.display='none'
+
+}
+
+)
